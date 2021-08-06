daa012a56f51f7061839aac3d0ea77b8b8861a3f
Nallon <nallonpauluzzi@hotmail.com>
2021-08-06 11:13:16 -0300

    Commit inicial

---
+++ src/main/java/one/digitalinnovation/dioproductcatalog/DioProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.dioproductcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioProductCatalogApplication.class, args);
+	}
+
+}
